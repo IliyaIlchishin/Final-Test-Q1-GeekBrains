630c1e9138612b8bd0d19bd282529505ce31abfd
«iliya <iliya.91f@gmail.com>
2022-10-18 22:46:38 +0300

    created skeleton

---
+++ Finaltest.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine($"Please enter text into array cell # {i}");
+        array [i] = System.Console.ReadLine();
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine($"# {i} - {array[i]} ");
+    }
+}
+
+
+string [] Words = new string[4]; 
+FillArray(Words); 
+PrintArray (Words);
+
+
+
