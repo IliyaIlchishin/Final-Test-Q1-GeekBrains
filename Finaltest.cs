ab9ff2abeab7130a0b0a56ef5244c9b1318805a5
«iliya <iliya.91f@gmail.com>
2022-10-18 23:08:51 +0300

    finished the task

--- Finaltest.cs
+++ Finaltest.cs
@@ -10,8 +10,31 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        System.Console.WriteLine($"# {i} - {array[i]} ");
+        if (array[i].Length <= 0)
+        {
+            System.Console.WriteLine();
+        }
+        else System.Console.WriteLine($"# {i} - {array[i]} ");
     }
+}
+
+string [] ThreeSignsArray (string [] array)
+
+{
+   
+    string [] NewArray = new string [array.Length];
+    int NewArrayIndex = 0 ;
+
+        for (int i=0; i<array.Length; i++)
+        {
+            if (array [i].Length <=3)
+            {
+                NewArray[NewArrayIndex] = array[i]; 
+                NewArrayIndex ++;
+            }
+
+        }
+    return NewArray;
 }
 
 
@@ -19,5 +42,7 @@ string [] Words = new string[4];
 FillArray(Words); 
 PrintArray (Words);
 
+System.Console.WriteLine();
 
-
+string [] ShortWords = ThreeSignsArray(Words); 
+PrintArray (ShortWords);
